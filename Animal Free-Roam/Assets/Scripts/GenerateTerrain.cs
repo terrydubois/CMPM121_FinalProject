a7f7bace82846c360332652e4d02debd4f30b4cb
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2019-03-09 19:51:30 -0800

    Trees

--- Animal Free-Roam/Assets/Scripts/GenerateTerrain.cs
+++ Animal Free-Roam/Assets/Scripts/GenerateTerrain.cs
@@ -9,17 +9,34 @@ using UnityEngine;
 namespace Suriyun {
     public class GenerateTerrain : MonoBehaviour
     {
+        List<GameObject> treeList = new List<GameObject>();
 
         void Start()
         {
             int heightScale = 10;
             float detailScale = 20.0f;
-
+            
             Mesh mesh = this.GetComponent<MeshFilter>().mesh;
             Vector3[] verts = mesh.vertices;
             for (int i = 0; i < verts.Length; i++) {
                 verts[i].y = Mathf.PerlinNoise((verts[i].x + this.transform.position.x) / detailScale,
                                                 (verts[i].z + this.transform.position.z) / detailScale) * heightScale;
+
+
+                if (verts[i].y > 6.9f && Mathf.PerlinNoise((verts[i].x+5) / 10, (verts[i].z + 5) / 10) * 10 > 4.6) {
+                    GameObject newTree = ScrTreePoolCreate.getTree();
+
+                    if (newTree != null) {
+                        Vector3 treePos = new Vector3(
+                            verts[i].x + this.transform.position.x,
+                            verts[i].y,
+                            verts[i].z + this.transform.position.z
+                        );
+                        newTree.transform.position = treePos;
+                        newTree.SetActive(true);
+                        treeList.Add(newTree);
+                    }
+                }
             }
 
             mesh.vertices = verts;
@@ -31,6 +48,16 @@ namespace Suriyun {
         void Update()
         {
             
+        }
+
+        void OnDestroy() {
+            for (int i = 0; i < treeList.Count; i++) {
+                if (myTrees[i] != null) {
+                    myTrees[i].SetActive(false);
+                }
+            }
+            
+            myTrees.Clear();
         }
     }
 }
---
+++ Animal Free-Roam/Assets/Scripts/ScrTreePoolCreate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrTreePoolCreate : MonoBehaviour
+{
+    static int treeAmount = 300;
+    public GameObject treePrefab;
+    static GameObject[] trees;
+    
+    void Start()
+    {
+        trees = new GameObject[treeAmount];
+        for (int i = 0; i < treeAmount; i++) {
+            trees[i] = (GameObject) Instantiate(treePrefab, Vector3.zero, Quaternion.identity);
+            trees[i].SetActive(false);
+        }
+    }
+
+    static public GameObject getTree() {
+
+        for (int i = 0; i < treeAmount; i++) {
+            if (!trees[i].activeSelf) {
+                return trees[i];
+            }
+        }
+        return null;
+    }
+
+    void Update()
+    {
+        
+    }
+}
