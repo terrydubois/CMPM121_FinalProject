1dc9a6a08503f3cf25569ab5767ea24c678dfef4
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2019-03-12 11:45:53 -0700

    Water V1

---
+++ Animal Free-Roam/Assets/ScrWaterMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrWaterMovement : MonoBehaviour
+{
+    public GameObject follow;
+    private Transform followTransform;
+    private Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        followTransform = follow.GetComponent<Transform>();
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position = new Vector3(followTransform.position.x,
+                                    myTransform.position.y,
+                                    followTransform.position.z);
+    }
+}
